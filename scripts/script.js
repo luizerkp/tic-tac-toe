b5edd0c2425c6d07a86aed10e1137f32b0a10a65
Luis Tamarez <luistamarez@hotmail.com>
2022-06-04 00:24:18 -0700

    Add simple logic to make random legal moves     * Refactored palyer moves to its own module     * Refactored computer moves to its own module     * Refactored display to its own module     TODO:         - Add AI algorithm to make computer moves         - clean up code         - difficulty level to match AI algorithm         - figure out how to find winner or draw

--- scripts/script.js
+++ scripts/script.js
@@ -37,10 +37,9 @@ var Selections = (function() {
             return location.reload();
         };
 
-        lockSelections(weaponSelection, difficultySelection, startButton);
-        displayBoard();
+        displayController.startDisplay(weaponSelection, difficultySelection, startButton);
 
-        return Game.start(weaponSelection.value, difficultySelection.value);
+        return game.start(weaponSelection.value, difficultySelection.value);
     });
 
     resetButton.addEventListener('click', function() {
@@ -49,85 +48,115 @@ var Selections = (function() {
 
 })();
 
-var Game = (function (){
-    let player;
-    let computer;
-    let difficulty;
-    
+var game = (function (){
+    // let difficulty;
     let winner = "";
-    let gameBoard = [];
-    let gameOver = false;
+    let gameBoard;
 
-    start = (chosenWeapon, chosenDifficulty) => {
+
+    const start = (chosenWeapon, chosenDifficulty) => {
         console.log(chosenWeapon, chosenDifficulty);
-        player = chosenWeapon;
-        computer = (chosenWeapon === 'x') ? 'o' : 'x';
-        difficulty = chosenDifficulty;
+        player.setPlayer(chosenWeapon);
+        computer.setComputer((chosenWeapon === 'x') ? 'o' : 'x');
+        // difficulty = chosenDifficulty;
+        gameBoard = board.createBoard();
+        // computer.setComputerMark(computer);
         addCellEvents();
-      
+        console.log(gameBoard);
     }
 
-    addCellEvents = () => {
+    const addCellEvents = () => {
         const cells = document.querySelectorAll('.cell');
         cells.forEach(cell => {
             cell.addEventListener('click', function() {
-                if (gameOver) {
+                if (checkGameOver()) {
+                    console.log('game over');
                     return;
                 }
                 if (cell.textContent !== '') {
+                    console.log(checkGameOver());
                     return;
-                }
-                const para = document.createElement('p');
-                para.textContent = player.toUpperCase();
-                para.classList.add('player-mark');
-                cell.appendChild(para);
-                // convert data to int for array index
-                // let row = parseInt(cell.getAttribute('data-row'));
-                // let col = parseInt(cell.getAttribute('data-col'));
-                // gameBoard[row][col].push(player);
-                // console.log(gameBoard);
+                }  
+
+                let row = cell.getAttribute('data-row');
+                let col = cell.getAttribute('data-col');
+
+                player.playerTurn(row, col);
+
 
                 // check game status
                 // checkWinnerHorizontally();
                 // checkWinnerVertically();
                 // checkWinnerDiagonally();
                 // checkDraw();
+            
+            
 
                 // allow the computer to make a move
-                // if (!gameOver) {
-                //     computerTurn();
-                // }
+                if (!checkGameOver()) {
+                    computer.computerTurn(gameBoard);
+                }
             });
         });
     }
-
-
-    checkWinnerHorizontally = () => {
-        // check for horizontal win
-    }
     
-    checkWinnerVertically = () => {
-        // check for vertical win
+    const updateBoard = (row, col, mark) => {
+        gameBoard[row][col] = mark;
+        console.log(gameBoard);
+    };
+
+    const checkGameOver = () => {
+        if (gameBoard.every(row => row.every(column => column !== null))) { 
+            console.log('draw');
+            return true;
+        }
+        return false;
     }
 
-    checkWinnerDiagonally = () => {
-        // check for diagonal win
-    }
 
-    checkDraw = () => {
-        // check for draw
-    }
+    // checkWinnerHorizontally = () => {
+    //     // check for horizontal win
+    // }
+    
+    // checkWinnerVertically = () => {
+    //     // check for vertical win
+    // }
 
-    end = () => {
-        // end game and declare winner
-    }
+    // checkWinnerDiagonally = () => {
+    //     // check for diagonal win
+    // }
+
+    // checkDraw = () => {
+    //     // check for draw
+    // }
+
+    // end = () => {
+    //     // end game and declare winner
+    // }
 
     return {
         start: start,
-    }
+        updateBoard: updateBoard
+    };
 
 })();
 
+var board = (function() {
+    let board = new Array(3).fill(null);
+
+    const createBoard = () => {
+        for (let i = 0; i < 3; i++) {
+            board[i] = new Array(3).fill(null);
+        }
+        return board;
+    }
+
+    return {
+        createBoard: createBoard
+    }
+})();
+
+// helper functions to check if user has changed the selection values via devtools
 function checkValidSelectionsValues(weaponScelectionValue, difficultySelectionValue) {
     let valid = true;
     
@@ -149,17 +178,99 @@ function checkValidSelectionsValues(weaponScelectionValue, difficultySelectionVa
     return valid;
 }
 
-function displayBoard () {
+var displayController = (function() {
     const board = document.querySelector('.board');
     const placeHolder = document.querySelector('#placeholder');
-    board.classList.toggle('inactive');
-    placeHolder.classList.toggle('inactive');
-}
+    
+    const startDisplay = (weaponSelection, difficultySelection, startButton) => {
+        displayBoard();
+        lockSelections(weaponSelection, difficultySelection, startButton);
+    }
 
-function lockSelections(weaponSelection, difficultySelection, startButton) {
-    weaponSelection.disabled = true;
-    difficultySelection.disabled = true;
-    startButton.disabled = true;
-}
+    const displayBoard = () => {
+        board.classList.toggle('inactive');
+        placeHolder.classList.toggle('inactive');
+    }
+
+    const lockSelections = (weaponSelection, difficultySelection, startButton) => {
+        weaponSelection.disabled = true;
+        difficultySelection.disabled = true;
+        startButton.disabled = true;
+    }
+    return {
+        startDisplay: startDisplay
+    }
+})();
+
+// test
+var player = (function() {
+    let playerMark;
+
+    const setPlayer = (playerMark) => {
+        this.playerMark = playerMark.toUpperCase();
+    }
+
+    const playerTurn = (row, col) => {
+        const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+        const para = document.createElement('p');
+        console.log(this.playerMark);
+        para.textContent = this.playerMark;
+        para.classList.add('player-mark');
+        cell.appendChild(para);
+        game.updateBoard(row, col, this.playerMark);
+    }
+    return {
+        setPlayer: setPlayer,
+        playerTurn: playerTurn
+    }
+
+})();
+
+
+var computer = (function() {
+   let computerMove;
+   let computerMark;
+    // let difficulty; // easy, medium, hard, impossible
+
+    const setComputer = (computerMark) => {
+        this.computerMark = computerMark.toUpperCase();
+        console.log(this.computerMark);
+    }
+
+    const computerTurn = (currentBoard) => {
+        // get computer move
+        computerMove = getComputerMove(currentBoard);
+        // display computer move
+        displayComputerMove(computerMove);
+    }
+
+    const getComputerMove = (currentBoard) => {
+        let row = Math.floor(Math.random() * 3);
+        let col = Math.floor(Math.random() * 3);
+
+        while (currentBoard[row][col] !== null) {
+            row = Math.floor(Math.random() * 3);
+            col = Math.floor(Math.random() * 3);
+        }
+
+        game.updateBoard(row, col, computerMark);
+
+        return [row, col];
+        
+    }
+
+    const displayComputerMove = (computerMove) => {
+        const cell = document.querySelector(`[data-row="${computerMove[0]}"][data-col="${computerMove[1]}"]`);
+        const para = document.createElement('p');
+        para.textContent = this.computerMark;
+        para.classList.add('computer-mark');
+        cell.appendChild(para);
+    }
+    return {
+        computerTurn: computerTurn,
+        setComputer: setComputer
+    };
+})();
+
 
 
