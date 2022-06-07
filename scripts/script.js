22158fe40fd7e3523b3ee7b4b45d3a9cebf1c1b1
Luis Tamarez <luistamarez@hotmail.com>
2022-06-07 00:52:19 -0700

    Add modal for play again prompt     * Add ability to see wich cells make a winning move     * Add logic to end game when a winner is found or tie is reached     * Add modal to html and css     * clean up code and make it more readable     * add delay of .5 seconds before displaying modal     *  TODO:         - Make computer smarter and implement minimax         - implement difficulty levels         - Add a delay before computer makes a move         - Test for bugs         - Revisit "playagain" prompt and general css look and feel

--- scripts/script.js
+++ scripts/script.js
@@ -34,7 +34,7 @@ var Selections = (function() {
 
         // if user changes the selection values using devtools, reload the page
         if (!checkValidSelectionsValues(weaponSelection.value, difficultySelection.value)) {
-            return location.reload();
+            return window.location.reload();
         };
 
         displayController.startDisplay(weaponSelection, difficultySelection, startButton);
@@ -50,31 +50,32 @@ var Selections = (function() {
 
 var game = (function (){
     // let difficulty;
-    let winner = "";
+
+    let winner = null;
+
+    // gameBoard is an array of 9 elements, each element is a string first dimension is row, second is column
+    // i.e. gameBoard[0][0] is the top left corner of the board
     let gameBoard;
 
 
     const start = (chosenWeapon, chosenDifficulty) => {
-        console.log(chosenWeapon, chosenDifficulty);
         player.setPlayer(chosenWeapon);
         computer.setComputer((chosenWeapon === 'x') ? 'o' : 'x');
         // difficulty = chosenDifficulty;
         gameBoard = board.createBoard();
-        // computer.setComputerMark(computer);
         addCellEvents();
-        console.log(gameBoard);
     }
 
     const addCellEvents = () => {
         const cells = document.querySelectorAll('.cell');
         cells.forEach(cell => {
             cell.addEventListener('click', function() {
-                if (checkGameOver()) {
-                    console.log('game over');
-                    return;
+                
+                if (checkGameOver() || winner !== null) {
+                    alert('oOPS! someone already won game will be reset');
+                    return window.location.reload();
                 }
                 if (cell.textContent !== '') {
-                    console.log(checkGameOver());
                     return;
                 }  
 
@@ -82,61 +83,69 @@ var game = (function (){
                 let col = cell.getAttribute('data-col');
 
                 player.playerTurn(row, col);
-
-
-                // check game status
-                // checkWinnerHorizontally();
-                // checkWinnerVertically();
-                // checkWinnerDiagonally();
-                // checkDraw();
-            
-            
+                winner = checkWinner.decideWinner(gameBoard);
 
                 // allow the computer to make a move
-                if (!checkGameOver()) {
+                if (!checkGameOver() && winner === null) {
                     computer.computerTurn(gameBoard);
+                    winner = checkWinner.decideWinner(gameBoard);
                 }
+
+                if (winner !== null) {
+                    if (winner !== "tie") {
+                    displayController.displayWinningCells();
+                    }
+
+                    end();
+                }
+
             });
         });
     }
     
     const updateBoard = (row, col, mark) => {
         gameBoard[row][col] = mark;
-        console.log(gameBoard);
     };
 
     const checkGameOver = () => {
+        // check if all cells are filled
         if (gameBoard.every(row => row.every(column => column !== null))) { 
-            console.log('draw');
             return true;
         }
         return false;
     }
 
 
-    // checkWinnerHorizontally = () => {
-    //     // check for horizontal win
-    // }
+    const end = async () => {
+        // wait .5 seconds before displaying the modal
+        await new Promise(resolve => setTimeout(resolve, 500));
+        // end game and declare winner
+        const modal = document.querySelector('.modal');
+        const playAgainBtn = document.querySelector('.playAgainBtn');
+        
+        let playAgainText = document.querySelector('.playAgainText');
     
-    // checkWinnerVertically = () => {
-    //     // check for vertical win
-    // }
-
-    // checkWinnerDiagonally = () => {
-    //     // check for diagonal win
-    // }
-
-    // checkDraw = () => {
-    //     // check for draw
-    // }
-
-    // end = () => {
-    //     // end game and declare winner
-    // }
+        if (player.getPlayerMark() === this.winner) {
+            playAgainText.textContent = "You Won!";
+        }
+        else if (computer.getComputerMark() === this.winner) {
+            playAgainText.textContent = "You Lost!";
+        }
+        else {
+            playAgainText.textContent = "It's a Tie!";
+        }
+    
+        modal.classList.toggle("show-modal");
+        
+        playAgainBtn.addEventListener('click', () => {
+            window.location.reload();
+        }); 
+    }
 
     return {
         start: start,
-        updateBoard: updateBoard
+        updateBoard: updateBoard,
+        checkGameOver: checkGameOver,
     };
 
 })();
@@ -197,44 +206,58 @@ var displayController = (function() {
         difficultySelection.disabled = true;
         startButton.disabled = true;
     }
+    const displayWinningCells = () => {
+        const winningCells = checkWinner.getWinningCells();
+        // console.log(winningCells);
+        const first = document.querySelector(`[data-row="${winningCells["first"][0]}"][data-col="${winningCells["first"][1]}"`);
+        const second = document.querySelector(`[data-row="${winningCells["second"][0]}"][data-col="${winningCells["second"][1]}"`);
+        const third = document.querySelector(`[data-row="${winningCells["third"][0]}"][data-col="${winningCells["third"][1]}"`);
+        first.classList.add('winning-cell');
+        second.classList.add('winning-cell');
+        third.classList.add('winning-cell');
+    }
+    
     return {
-        startDisplay: startDisplay
+        startDisplay: startDisplay,
+        displayWinningCells: displayWinningCells,
     }
 })();
 
 // test
 var player = (function() {
-    let playerMark;
+    let playerMark = null;
 
-    const setPlayer = (playerMark) => {
-        this.playerMark = playerMark.toUpperCase();
+    const setPlayer = (mark) => {
+        playerMark = mark.toUpperCase();
     }
 
     const playerTurn = (row, col) => {
         const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
         const para = document.createElement('p');
-        console.log(this.playerMark);
-        para.textContent = this.playerMark;
+        para.textContent = playerMark;
         para.classList.add('player-mark');
         cell.appendChild(para);
-        game.updateBoard(row, col, this.playerMark);
+        game.updateBoard(row, col, playerMark);
+    }
+    const getPlayerMark = () => {
+        return playerMark;
     }
     return {
         setPlayer: setPlayer,
-        playerTurn: playerTurn
+        playerTurn: playerTurn,
+        getPlayerMark: getPlayerMark
     }
 
 })();
 
 
 var computer = (function() {
-   let computerMove;
-   let computerMark;
+   let computerMove = null;
+   let computerMark = null;
     // let difficulty; // easy, medium, hard, impossible
 
-    const setComputer = (computerMark) => {
-        this.computerMark = computerMark.toUpperCase();
-        console.log(this.computerMark);
+    const setComputer = (mark) => {
+        computerMark = mark.toUpperCase();
     }
 
     const computerTurn = (currentBoard) => {
@@ -262,14 +285,120 @@ var computer = (function() {
     const displayComputerMove = (computerMove) => {
         const cell = document.querySelector(`[data-row="${computerMove[0]}"][data-col="${computerMove[1]}"]`);
         const para = document.createElement('p');
-        para.textContent = this.computerMark;
+        para.textContent = computerMark;
         para.classList.add('computer-mark');
         cell.appendChild(para);
     }
+
+    const getComputerMark = () => {
+        return computerMark;
+    }
+
     return {
         computerTurn: computerTurn,
-        setComputer: setComputer
+        setComputer: setComputer,
+        getComputerMark: getComputerMark
     };
+})();
+
+var checkWinner = (function() {
+    let winner = null;
+    const cells = ["first", "second" , "third"];
+    let winningCells = {
+        first: null,
+        second: null,
+        third: null,
+    }
+    
+    const decideWinner = (currentBoard) => {
+        // check for horizontal win
+        checkHorizontalWin(currentBoard);
+
+        // check for vertical win
+        checkVerticalWin(currentBoard);
+        
+        // check for diagonal win
+        checkDiagonalWin(currentBoard);
+        
+        // check for draw
+        checkDraw(currentBoard);
+
+        return winner;
+        
+    }
+
+    // check for horizontal win
+    const checkHorizontalWin = (currentBoard) => {   
+        // i = row
+        for (let i = 0; i < 3; i++) {
+            if (currentBoard[i][0] === currentBoard[i][1] && currentBoard[i][1] === currentBoard[i][2]) {
+                winner = currentBoard[i][0];
+
+                let j = 0;
+                while (j < 3) {
+                    winningCells[cells[j]] = [i, j];
+                    j++;
+                }
+            }
+        }
+    };
+
+    // check for vertical win
+    const checkVerticalWin = (currentBoard) => {
+        // i = column   
+        for (let i = 0; i < 3; i++) {
+            if (currentBoard[0][i] === currentBoard[1][i] && currentBoard[1][i] === currentBoard[2][i]) {
+                winner = currentBoard[0][i];
+
+                let j = 0;
+                while (j < 3) {
+                    winningCells[cells[j]] = [j, i];
+                    j++;
+                }
+            }
+        }
+    }
+
+    // check for diagonal win
+    const checkDiagonalWin = (currentBoard) => {
+        if (currentBoard[0][0] === currentBoard[1][1] && currentBoard[1][1] === currentBoard[2][2]) {
+            winner = currentBoard[0][0];
+
+            let j = 0;
+            while (j < 3) {
+                winningCells[cells[j]] = [j, j];
+                j++;
+            }
+        } 
+        // check for diagonal win top right to bottom left
+        else if (currentBoard[0][2] === currentBoard[1][1] && currentBoard[1][1] === currentBoard[2][0]) {
+            winner = currentBoard[0][2];
+
+            let j = 0;
+            while (j < 3) {
+                winningCells[cells[j]]= [j, (2 - j)];
+                j++;
+            }
+        } 
+    }
+
+    // check for draw
+    const checkDraw = () => {
+        // draw = game is not over and no winner
+        if (game.checkGameOver() && winner === null) {
+            winner = 'tie';
+        }
+    }
+
+    const getWinningCells = () => {
+        return winningCells;
+    }
+
+    return {
+        decideWinner: decideWinner,
+        getWinningCells: getWinningCells
+    }
+
 })();
 
 
